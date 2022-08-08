9dfc14e545981fa09c48127818cf00f3dabe2db9
Rodrigo Vidal <rodrigo.vidal@b2wdigital.com>
2022-08-08 17:21:15 -0300

    Commit inicial

---
+++ src/main/kotlin/com/elo7/desafio/DesafioApplication.kt
@@ -0,0 +1,11 @@
+package com.elo7.desafio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DesafioApplication
+
+fun main(args: Array<String>) {
+	runApplication<DesafioApplication>(*args)
+}
---
+++ src/test/kotlin/com/elo7/desafio/DesafioApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.elo7.desafio
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DesafioApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
