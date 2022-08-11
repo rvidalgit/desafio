ffcd36b4b72a61f161bb4cfd5a30562e2b961b53
Rodrigo Vidal <rodrigo.vidal@b2wdigital.com>
2022-08-10 23:42:42 -0300

    Config dos servers na doc Descrição dos endpoints na documentação

--- src/main/kotlin/com/elo7/desafio/config/swagger/SwaggerConfig.kt
+++ src/main/kotlin/com/elo7/desafio/config/swagger/SwaggerConfig.kt
@@ -3,6 +3,7 @@ package com.elo7.desafio.config.swagger
 import io.swagger.v3.oas.models.OpenAPI
 import io.swagger.v3.oas.models.info.Contact
 import io.swagger.v3.oas.models.info.Info
+import io.swagger.v3.oas.models.servers.Server
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -12,16 +13,26 @@ class SwaggerConfig {
     @Bean
     fun openApi(): OpenAPI =
         OpenAPI()
-            .info(
-                Info().title("Desafio Elo7")
-                    .description("API desenvolvida para o desafio")
-                    .version("v1")
-                    .contact(
-                        Contact()
-                            .email("rodrigovidal2006@gmail.com")
-                            .name("Rodrigo Vidal")
-                            .url("https://github.com/rvidalgit/desafio")
-                    )
+            .servers(getServers())
+            .info(getInfo())
+
+    private fun getInfo(): Info {
+        return Info().title("Desafio Elo7")
+            .description("API desenvolvida para o desafio")
+            .version("v1")
+            .contact(
+                Contact()
+                    .email("rodrigovidal2006@gmail.com")
+                    .name("Rodrigo Vidal")
+                    .url("https://github.com/rvidalgit/desafio")
             )
+    }
+
+    private fun getServers(): MutableList<Server> {
+        return mutableListOf(
+            Server().url("http://localhost:8080").description("Development server"),
+            Server().url("https://aws.com").description("Production server")
+        )
+    }
 
 }
--- src/main/kotlin/com/elo7/desafio/planet/controller/PlanetController.kt
+++ src/main/kotlin/com/elo7/desafio/planet/controller/PlanetController.kt
@@ -2,7 +2,13 @@ package com.elo7.desafio.planet.controller
 
 import com.elo7.desafio.planet.model.Planet
 import com.elo7.desafio.planet.service.PlanetService
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.dao.EmptyResultDataAccessException
+import org.springframework.data.domain.Page
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -15,6 +21,14 @@ class PlanetController(
     val planetService: PlanetService
 ) {
 
+    @Operation(summary = "Responsável por criar um planeta")
+    @ApiResponse(
+        content = [Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = Planet::class)
+        )],
+        responseCode = "201"
+    )
     @PostMapping
     fun createPlanet(@Valid @RequestBody planet: Planet): ResponseEntity<Planet> {
         val planetSave = planetService.create(planet)
@@ -23,11 +37,27 @@ class PlanetController(
             .body(planetSave)
     }
 
+    @Operation(summary = "Responsável por retornar um planeta")
+    @ApiResponses(
+        ApiResponse(responseCode = "200", description = "Planeta encontrado", content = [Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = Planet::class)
+        )]),
+        ApiResponse(responseCode = "404", description = "Planeta não encontrado", content = [Content()])
+    )
     @GetMapping("/{id:\\d+}")
     fun get(@PathVariable id: Long): ResponseEntity<Planet> {
         return ResponseEntity.of(planetService.get(id))
     }
 
+    @Operation(summary = "Responsável por retornar uma lista paginada de planetas")
+    @ApiResponse(
+        content = [Content(
+            mediaType = "application/json",
+            schema = Schema(implementation = Page::class)
+        )],
+        responseCode = "200"
+    )
     @GetMapping
     @ResponseStatus(HttpStatus.OK)
     fun list(
@@ -37,6 +67,11 @@ class PlanetController(
         return planetService.list(page, size)
     }
 
+    @Operation(summary = "Responsável por remover um planeta e suas sondas")
+    @ApiResponses(
+        ApiResponse(responseCode = "202", description = "Deleção aceita", content = [Content()]),
+        ApiResponse(responseCode = "404", description = "Planeta não encontrado", content = [Content()])
+    )
     @DeleteMapping("/{id:\\d+}")
     fun delete(@PathVariable id: Long): ResponseEntity<Any> {
         return try {
