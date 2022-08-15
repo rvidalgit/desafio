652e9e78dcfdf2798a709fca404d9c6fddde5eb4
Rodrigo Vidal <rodrigo.vidal@b2wdigital.com>
2022-08-14 21:25:58 -0300

    Add driver postgresql

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	runtimeOnly("com.h2database:h2")
+	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
 	testImplementation("org.springframework.boot:spring-boot-starter-test"){
 		exclude(module = "mockito-core")
