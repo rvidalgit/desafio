db82c5665b72997de30c11cdf05b3c2098b1e83c
Rodrigo Vidal <rodrigo.vidal@b2wdigital.com>
2022-08-15 21:11:49 -0300

    Arrumando ordem dos campos

--- src/main/resources/db/migration/V1.0__create_databases.sql
+++ src/main/resources/db/migration/V1.0__create_databases.sql
@@ -9,24 +9,24 @@ create sequence space_probe_seq start with 1 increment by 1;
 create table planet
 (
     id         bigint  not null primary key,
-    created_at timestamp,
     height     integer not null,
-    updated_at timestamp,
-    width      integer not null
+    width      integer not null,
+    created_at timestamp,
+    updated_at timestamp
 );
 
 create table space_probe
 (
-    id         bigint       not null primary key,
-    created_at timestamp,
+    id         bigint     not null primary key,
     direction  varchar(5) not null,
-    x          integer      not null,
-    y          integer      not null,
-    updated_at timestamp,
-    planet_id  bigint       not null
+    x          integer    not null,
+    y          integer    not null,
+    planet_id  bigint     not null
         constraint FK_PROBE_PLANET
             references planet
-            on delete cascade
+            on delete cascade,
+    created_at timestamp,
+    updated_at timestamp
 );
 
 alter table space_probe
